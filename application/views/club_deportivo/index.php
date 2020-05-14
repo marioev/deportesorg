20fff8f9f2a5e8a1b714725a0dcf5c1c8a4eb628
alejandro <ale.hv@gmail.com>
2020-05-14 11:36:33 -0400

    para push

--- application/views/club_deportivo/index.php
+++ application/views/club_deportivo/index.php
@@ -2,7 +2,7 @@
     <div class="col-md-12">
         <div class="box">
             <div class="box-header">
-                <h3 class="box-title">Club Deportivo Listing</h3>
+                <h3 class="box-title">Club Deportivo</h3>
             	<div class="box-tools">
                     <a href="<?php echo site_url('club_deportivo/add'); ?>" class="btn btn-success btn-sm">Add</a> 
                 </div>
